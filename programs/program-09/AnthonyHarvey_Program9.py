ad8f0e2657b65054ae92e2e3bfc8cab041227a88
Anthony Harvey <tonyharvey85@gmail.com>
2018-07-17 17:29:16 -0500

    Adding current content

---
+++ programs/program-09/AnthonyHarvey_Program9.py
@@ -0,0 +1,84 @@
+#********************************************************************
+#
+#  Author:            <Your name>
+#
+#  Program #:         9
+#
+#  File Name:         Program9.py
+#
+#  Course:            COSC 1336 Programming Fundamentals I 
+#
+#  Due Date:          <Due Date>
+#
+#  Instructor:        Fred Kumi 
+#
+#  Chapter:           <Chapter #>
+#
+#  Description:
+#     This program reads and displays the contents
+#     of a file.
+#********************************************************************
+
+###########################################################################
+#
+# This is just an outline of what I need to write for Program 9.
+# 
+# End Goals:
+#    Total rainfall for the year
+#    Average Monthly rainfall
+#    Highest rainfall month
+#    Lowest rainfall month
+#
+# Stipulations:
+#    No global variables
+#    At least 4 functions
+#    Main() should drive the functions, no calculations within main()
+#
+# Rough Draft / Outline of the program:
+#
+############################################################################
+
+def main():
+    userInput()
+    rainfall_List_Accumulator = data_Input()
+    highest_Rainfall, lowest_Rainfall, = rainfall_Calculations()
+
+
+def data_Input():
+    rainfall_List = []
+    rainfall_List_Accumulator = 0
+
+    data = open('Program9.txt', 'r')
+    line_Read = data.readline()
+    while line_Read != '':
+        words = line_Read.split()
+        print("Words Debug:", words)
+        #words_Fix= float(words.replace("'"))
+        #rainfall_List_Accumulator += words_Fix
+        # This seems to have an issue. Float() cant handle the data type here.
+        
+        # Create a list of months and compare the indices to the text files monthly listings.
+        # Then return those values to main(), where it will then be forwarded to the
+        # rainfall_Calculations function.
+        #print("After rainfall_accumulator Debugging:", words)
+        line_Read = data.readline()
+        rainfall_List_Accumulator = 100
+
+    return(rainfall_List_Accumulator)
+ 
+def rainfall_Calculations():
+    avg_Monthly = rainfall_List_Accumulator / 12
+    highest_Rainfall = index(max(rainfall_List))
+    lowest_Rainfall = index(min(rainfall_List))
+        
+
+def data_Output():
+    print("Blah blah:", rainfall_List_Accumulator)
+    print("Blah Blah:", avg_Monthly)
+    print("Blah:", highest_Rainfall)
+    print("Blah:", lowest_Rainfall)
+
+def userInput():
+	print("Anthony Harvey, 2018-07-17")
+
+main()
