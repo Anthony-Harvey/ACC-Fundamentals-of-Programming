2ec650cb8592b50535436d38054da68eb3e1bd57
Anthony Harvey <tonyharvey85@gmail.com>
2018-07-18 20:34:50 -0500

    Adding completed Program-09 as well as more information on Program-11 and Program-12

---
+++ programs/program-09/AnthonyHarvey_Program9-Output.py
@@ -0,0 +1,31 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Verif\Desktop\ACC 2018\Programs\AnthonyHarvey_Program9\Program9-Custom2.py 
+Name:     Anthony Harvey
+Course:   Programming Fundamentals I
+Program:  Nine
+
+-------------------------------------------------------------------------
+The supplied rainfall data that is used for this program:
+
+1.89
+1.99
+2.14
+2.51
+5.03
+3.81
+1.97
+2.31
+2.91
+3.97
+2.68
+2.44
+-------------------------------------------------------------------------
+
+Based on the supplied information, the following observations can be made:
+
+The average monthly rainfall is 2.80 inches of rain
+The month with the lowest rainfall amount was January with 1.89 inches of rain
+The month with the highest rainfall amount was May with 5.03 inches of rain
+>>> 
--- programs/program-09/AnthonyHarvey_Program9.py
+++ programs/program-09/AnthonyHarvey_Program9.py
@@ -1,6 +1,6 @@
 #********************************************************************
 #
-#  Author:            <Your name>
+#  Author:            Anthony Harvey
 #
 #  Program #:         9
 #
@@ -8,77 +8,114 @@
 #
 #  Course:            COSC 1336 Programming Fundamentals I 
 #
-#  Due Date:          <Due Date>
+#  Due Date:          2018-07-16
 #
 #  Instructor:        Fred Kumi 
 #
-#  Chapter:           <Chapter #>
+#  Chapter:           5, 6, and 7
 #
 #  Description:
 #     This program reads and displays the contents
-#     of a file.
+#     of a file. Then makes rainfall calculations based on it.
 #********************************************************************
 
-###########################################################################
+#***************************************************************
 #
-# This is just an outline of what I need to write for Program 9.
+#  Function:            main
 # 
-# End Goals:
-#    Total rainfall for the year
-#    Average Monthly rainfall
-#    Highest rainfall month
-#    Lowest rainfall month
+#  Description:         The main function of the program
 #
-# Stipulations:
-#    No global variables
-#    At least 4 functions
-#    Main() should drive the functions, no calculations within main()
+#  Parameters:          None
 #
-# Rough Draft / Outline of the program:
+#  Returns:             Nothing 
 #
-############################################################################
+#**************************************************************
 
 def main():
-    #userInput()
-    data_Input()
-    #rainfall_List_Accumulator = data_Input()
-    highest_Rainfall, lowest_Rainfall, = rainfall_Calculations()
-    #data_Output()
-
+    userInfo()
+    sum_Rainfall, rainfall_List = data_Input()
+    #   Call the data_Input function and collect the returned data
+    avg_Monthly, lowest_Rainfall, lowest_Month, highest_Rainfall, highest_Month = rainfall_Calculations(sum_Rainfall, rainfall_List)
+    #   Call the rainfall_Calculations function with sum_Rainfall and rainfall_List used as arguments
+    #   and collects the returned data for use in the following data_Output call
+    data_Output(avg_Monthly, lowest_Rainfall, lowest_Month, highest_Rainfall, highest_Month)
+    
 def data_Input():
     rainfall_List = []
-    rainfall_List_Accumulator = 0
+    #   Creating the list that will contain data read from the 'Program9.txt' file
 
+    print("-------------------------------------------------------------------------")
+    print("The supplied rainfall data that is used for this program:")
+    print()
+    
     data = open('Program9.txt', 'r')
     line_Read = data.readline()
     while line_Read != '':
         words = line_Read.split()
-        words_Fix = float(words[0])
-        print(words_Fix)
-        rainfall_List_Accumulator += words_Fix
+        monthly_Rainfall = float(words[0])
+        print(monthly_Rainfall)
         
-        # Create a list of months and compare the indices to the text files monthly listings.
-        # Then return those values to main(), where it will then be forwarded to the
-        # rainfall_Calculations function
-
+        rainfall_List.append(monthly_Rainfall)
+        sum_Rainfall = sum(rainfall_List)
+        #   Append each iteration to a list and then sum the lists contents
+        
         line_Read = data.readline()
 
-    print(rainfall_List_Accumulator)
-    #return(rainfall_List_Accumulator)
- 
-def rainfall_Calculations():
-    #avg_Monthly = rainfall_List_Accumulator / 12
-    #highest_Rainfall = index(max(rainfall_List))
-    #lowest_Rainfall = index(min(rainfall_List))
-        
+    return(sum_Rainfall, rainfall_List)
 
-#def data_Output():
-    #print("Blah blah:", rainfall_List_Accumulator)
-    #print("Blah Blah:", avg_Monthly)
-    #print("Blah:", highest_Rainfall)
-    #print("Blah:", lowest_Rainfall)
-    #print("data_Output test!")
 
-#def userInput():
-    #print("Anthony Harvey - 2018-07-17")
+def rainfall_Calculations(sum_Rainfall, rainfall_List):
+    #   This function will do the bulk of the work
+    #   It will accept the rainfall_List and sum_Rainfall arguments from main()
+    #   It will then calculate the Average Monthly rainfall
+    #   Then find the months with the Highest and Lowest rainfall totals
+    #
+    #   It then returns the relevant data to main()
+
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    avg_Monthly = sum_Rainfall / 12
+    
+    highest_Rainfall = max(rainfall_List)
+    lowest_Rainfall = min(rainfall_List)
+
+    lowest_Index = rainfall_List.index(lowest_Rainfall)
+    highest_Index = rainfall_List.index(highest_Rainfall)
+    #   These two give me index values that will help me match the corresponding month in the months list
+    
+    lowest_Month = months[lowest_Index]
+    highest_Month = months[highest_Index]
+    #   The corresponding months have now been identified within the months list
+
+    return(avg_Monthly, lowest_Rainfall, lowest_Month, highest_Rainfall, highest_Month)
+
+def data_Output(avg_Monthly, lowest_Rainfall, lowest_Month, highest_Rainfall, highest_Month):
+    print("-------------------------------------------------------------------------")
+    print()
+    print("Based on the supplied information, the following observations can be made:")
+    print()
+    print("The average monthly rainfall is", format(avg_Monthly, '.2f'), 'inches of rain')
+    print("The month with the lowest rainfall amount was", lowest_Month, 'with', lowest_Rainfall, "inches of rain")
+    print("The month with the highest rainfall amount was", highest_Month, 'with', highest_Rainfall, "inches of rain")
+
+
+#***************************************************************
+#
+#  Function:     userInfo
+# 
+#  Description:  Prints Programmer's information
+#
+#  Parameters:   None
+#
+#  Returns:      Nothing 
+#
+#**************************************************************
+def userInfo():
+    print('Name:     Anthony Harvey')
+    print('Course:   Programming Fundamentals I')
+    print('Program:  Nine')
+    print()
+    # End of userInfo
+
+# End of main
 main()
