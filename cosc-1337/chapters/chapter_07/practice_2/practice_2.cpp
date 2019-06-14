e1febeb2f91d4da540f5b20d2bf0bbcfba354de7
Anthony Harvey <tonyharvey85@gmail.com>
2019-06-14 01:04:03 -0500

    Updating practice file..Currently working on Lab2. I'll rename the file and move its location momentarily.

--- cosc-1337/chapters/chapter_07/practice_2/practice_2.cpp
+++ cosc-1337/chapters/chapter_07/practice_2/practice_2.cpp
@@ -1,10 +1,17 @@
 //	Lab 2
 //	Chapter 6: Problem 15
 
-	//	The Middletown Whlesale Copper Wire Comany sells spools of copper wiring for 100 each and ships them for 10 each.
+	//	The Middletown Whlesale Copper Wire Company sells:
+	//		spools of copper wiring for 100 each
+	//	Shipping Cost:
+	//		ships them for 10 each.
+	//
+	//
 	//	Write a program that displays the status of an order.
 	//	
-	//	It should use two functions. The first function asks for the following data and stores the input values in reference paramters.
+	//	It should use two functions..
+	//	
+	//	The first function asks for the following data and stores the input values in reference paramters.
 	//		Spools: Number_of_Ordered
 	//		Spools:	Number_in_Stock
 	//		Any Special shipping and handling Charges
@@ -28,47 +35,81 @@
 //	
 //	Use pass by values/refernces when calling functions
 
-#include <iostream>;
-#include <array>;
+#include <iostream>
 
 using namespace std;
 
-const cost_of_base_items = 0
-const cost_of_base_items = 0
+const int costBaseItem = 100;
+const int costShipping = 10;
 
 //	Function Prototypes might be good here
-//		This would maybe help identify variables containing
+//		This would maybe helps identify variables containing
 //		pass by references/values.
-
+//		And keeps the main() function at the top for easier readability
+void inventoryFunction(int &stocked, int &ordered, double &specialChgs);
 
 int main()
 {
-	int numOrdered =0,
-		inStock = 0;
+	int	stocked = 0;
+	int	ordered = 0;
 	double specialChgs = 0.0;
 	
+	cout << "Software owned and operated by Middletown Wholesale Copper Wire Company." << endl;
+	cout << "\tWe've got copper and you've got problems!\n" << endl;
+	cout << "WARNING: " << endl;
+	cout << "\tThis software is restricted by the Arms Export Control Act, " << endl;
+	cout << "\tTitle 22 USC Sec 2751 et seq." << endl;
+	cout << "\tViolations of export laws are subject to severe criminal penalties!" << endl;
+	cout << "\tHave a nice day!\n\n" << endl;
+
+	cout << "Please answer the following questions: \n" << endl;
+	cout << "\tHow many Copper Wire Spools do we currently have in stock?\n\t";
+	cin >> stocked;
+	cout << "\tHow many Copper Wire Spools have been ordered?\n\t";
+	cin >> ordered;
+	cout << "\tPlease input any additional Special Shipping and Handling charges: \n\t";
+	cout << "(Enter 0 if none)\n\t";
+	cin >> specialChgs;
 	
 	
-	inventoryFunction(getOrder, etc, etc)
+	inventoryFunction(stocked, ordered, specialChgs);
 	
-	
-	
+	system("Please press ANY key to close this window.");
 	return 0;
 }
 
-void inventoryFunction(int &getOrder, int $etc, double &etc)
+void inventoryFunction(int &stocked, int &ordered, double &specialChgs)
 {
+	int outboundStock = 0;
+	int stockNeeded = 0;
+	int handlingCost = 0;
+	double gross = 0.0;
+	double handling = 0.0;
 	
-	while (something)
+	if (ordered > stocked)
 	{
-		//	Do Something
+		stockNeeded = stocked - ordered;
+		cout << "Uhhh somebody should order more stock so we can fill these orders..." << endl;
+		cout << "More specifically..";
+		cout << "We need at least " << abs(stockNeeded);
+		cout << " more items to fill the current order list!" << endl;
 	}
-		
-	while (something)
-	{
-		//	Do something
-		
-	}
+	cout << "--------------------------------------------------------------------" << endl;
+	cout << "--\t\tCurrent Inventory Status\t\t          --" << endl;
+	cout << "--\tIn Stock\tNumber Ordered\t\tNumber Needed\t  --" << endl;
+	cout << "--\t\t\t\t\t\t\t\t  --" << endl;
+	cout << "--\t\t\t\t\t\t\t\t  --" << endl;
+	cout << "--\t\t\t\t\t\t\t\t  --" << endl;
+	cout << "--\t\t\t\t\t\t\t\t  --" << endl;
+	cout << "--\t\t\t\t\t\t\t\t  --" << endl;
+	cout << "--------------------------------------------------------------------" << endl;
+	cout << stocked << ordered << specialChgs <<  endl;
+	cout << outboundStock << stockNeeded << handlingCost << gross << handling << endl;
+	
+	
+	
+	
+	
 	
 	
 }
